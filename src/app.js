2355b1f5ade581bbd330f59ab95401579523ae78
Tasha Perry <nata1031986@gmail.com>
2023-09-24 21:03:49 +0300

    Add java

--- src/app.js
+++ src/app.js
@@ -1,20 +1,6 @@
-
-function shareWeather() {
-    // Check if the Web Share API is supported
-    if (navigator.share) {
-        navigator.share({
-            title: 'Check out the weather',
-            text: 'Current weather in Denver, USA: 21°C, Cloudy',
-            url: 'https://example.com/weather' // Replace with your app's URL
-        })
-        .then(() => console.log('Shared successfully'))
-        .catch((error) => console.error('Sharing failed', error));
-    } else {
-        // Fallback behavior for browsers that do not support the Web Share API
-        alert('Sharing is not supported in this browser.');
-    }
-}
-
-// Add a click event listener to the share button
-document.getElementById('share-button').addEventListener('click', shareWeather);
-
+  document.getElementById('share-button').addEventListener('click', function () {
+    var subject = 'Check out this awesome website';
+    var body = 'I found this awesome website: https://example.com';
+    var mailtoLink = 'mailto:?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+    window.location.href = mailtoLink;
+  });
