6b457512bf6d320ab59d76417a1f47e8697d8f9c
Tasha Perry <nata1031986@gmail.com>
2023-09-27 10:04:17 +0300

    Working on Java

--- src/app.js
+++ src/app.js
@@ -1,6 +1,60 @@
-  document.getElementById('share-button').addEventListener('click', function () {
-    var subject = 'Check out this awesome website';
-    var body = 'I found this awesome website: https://example.com';
-    var mailtoLink = 'mailto:?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
-    window.location.href = mailtoLink;
-  });
+  
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector('#temperature');
+  let cityElement = document.querySelector("#cityName");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+ let celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+let apiKey = '843fed47f3a58c43ftc397aabb8d5oc0';
+let apiURL = 'https://api.shecodes.io/weather/v1/current?query=Paris&key=843fed47f3a58c43ftc397aabb8d5oc0&units=metric';
+axios.get(apiUrl).then(displayTemperature);
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function search(city) {
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
